4152a243fbe4c9376e1180f0763f8a5ec01ad922
gabrielZS1 <gabrieldev2701@gmail.com>
2025-12-03 13:40:51 -0300

    Tela de Menu concluida

---
+++ PetCenterPTCC/Controllers/Agenda.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetCenterPTCC.Controllers
+{
+    public class Agenda : Controller
+    {
+        public IActionResult NossaAgenda()
+        {
+            return View();
+        }
+    }
+}
---
+++ PetCenterPTCC/Controllers/Servicos.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetCenterPTCC.Controllers
+{
+    public class Servicos : Controller
+    {
+        public IActionResult NossosServicos()
+        {
+            return View();
+        }
+    }
+}
